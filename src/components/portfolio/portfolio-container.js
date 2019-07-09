86db2228de9ea70064c9c199d2ce064d5763b865
CADEN MEYER <meyerca1@wsd.ad>
2019-07-09 09:13:56 -0600

    Nav Bar

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -6,21 +6,55 @@ export default class PortfolioContainer extends Component{
     constructor(){
         super();
 
-        console.log("Portfolio container has rendered");
+        this.state = {
+            pageTitle: "Welcome to the Jungle",
+            data: [
+                {title: "Quip", category: "eCommerce"},
+                {title: "Eventbrite", category: "Scheduling"},
+                {title: "Ministry Safe", category: "Enterprise"},
+                {title: "Swingaway", category: "eCommerce"}
+            ]
+        };
+
+
+        this.handleFilter = this.handleFilter.bind(this);
+    }
+
+    handleFilter(filter) {
+        this.setState({
+            data: this.state.data.filter(item => {
+                return item.category === filter;
+            })
+        });
     }
 
     portfolioItems(){
-        const data = ["Quip","Eventbrite","Ministry Safe","Swingaway"];
 
-        return data.map(item => {
-            return <PortfolioItem title={item} />;
-        })
+        return this.state.data.map(item => {
+            return <PortfolioItem title={item.title} url={"google.com"} />;
+        });
     }
+
+    // handlePageTitleUpdate(){
+    //     this.setState({
+    //         pageTitle: "Something Else"
+    //     });
+    // }
 
     render(){
         return(
             <div>
-            <h2>Portfolio items go here...</h2>  
+            <h2>{this.state.pageTitle}</h2>  
+
+            <button onClick={() => this.handleFilter('eCommerce')}>
+            eCommerce
+            </button>
+            <button onClick={() => this.handleFilter('Scheduling')}>
+            Scheduling
+            </button>
+            <button onClick={() => this.handleFilter('Enterprise')}>
+            Enterprise
+            </button>
 
             {this.portfolioItems()}
             </div>
