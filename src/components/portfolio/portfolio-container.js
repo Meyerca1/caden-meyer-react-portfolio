204c0f468171212ab4316053f8f272a40515f22a
CADEN MEYER <meyerca1@wsd.ad>
2019-07-09 09:36:16 -0600

    Nav Container

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -8,6 +8,7 @@ export default class PortfolioContainer extends Component{
 
         this.state = {
             pageTitle: "Welcome to the Jungle",
+            isLoading: false,
             data: [
                 {title: "Quip", category: "eCommerce"},
                 {title: "Eventbrite", category: "Scheduling"},
@@ -42,6 +43,9 @@ export default class PortfolioContainer extends Component{
     // }
 
     render(){
+        if (this.state.isLoading){
+            return <div>Loading...</div>;
+        }
         return(
             <div>
             <h2>{this.state.pageTitle}</h2>  
