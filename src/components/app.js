dc23ca5a8a75668112c9851b7eef7c9692e855f8
CADEN MEYER <meyerca1@wsd.ad>
2019-07-15 11:12:45 -0600

    authorization

--- src/components/app.js
+++ src/components/app.js
@@ -11,6 +11,24 @@ import Auth from './pages/auth';
 import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN"
+    }
+  }
+
+  handleSuccessfulLogin(){
+    this.setState({
+      loggedInStatus: "LOGGED_IN"
+    })
+  }
+  handleUnSuccessfulLogin(){
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    })
+  }
   render() {
     return (
       <div className='container'>
@@ -22,7 +40,18 @@ export default class App extends Component {
 
             <Switch>
               <Route exact path="/" component={Home} />
-              <Route path="/auth" component={Auth} />
+
+              <Route 
+              path="/auth" 
+              render={props => (
+                <Auth
+                  {...props}
+                  handleSuccessfulLogin={this.handleSuccessfulLogin}
+                  handleUnSuccessfulLogin={this.handleUnSuccessfulLogin}
+                  />
+              )}
+              />
+
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
