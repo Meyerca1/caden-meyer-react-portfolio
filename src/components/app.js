2ac1cb34ed6cb6f2261be0c541e69517279ba245
CADEN MEYER <meyerca1@wsd.ad>
2019-07-16 11:40:22 -0600

    portfolio manager

--- src/components/app.js
+++ src/components/app.js
@@ -7,7 +7,7 @@ import Home from './pages/home';
 import About from './pages/about';
 import Contact from './pages/contact';
 import Blog from './pages/blog';
-import PortfolioManager from "./pages/portfolio-manager";
+import PortfolioManager from './pages/portfolio-manager';
 import PortfolioDetail from './portfolio/portfolio-detail';
 import Auth from './pages/auth';
 import NoMatch from "./pages/no-match";
--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export default class PortfolioManager extends Component {
+    constructor (){
+        super();
+
+        this.state={}
+    }
+
+    updatePortfolioItems(){
+        return axios.get("https://cadenmeyer.devcamp.space/portfolio/portfolio_items")
+        .then(response => {
+        this.setState({
+            data: response.data.portfolio_items
+        });
+    }).catch(error => {
+        console.log(error);
+    });
+    }
+
+
     render() {
         return (
             <div className = "portfolio-manager-wrapper">
@@ -8,7 +27,8 @@ export default class PortfolioManager extends Component {
                     <h1>Portfolio form...</h1> 
                 </div>
                 <div className="right-column">
-                    <h1>Portfolio Sidebar</h1>     
+                    <h1>Portfolio Sidebar</h1>  
+                    {this.data}   
                 </div>
 
             </div>
