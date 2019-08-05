587a71346e9f1d89d5c3226abc8a3c0888fa4a4c
Michelle <dillonmeyer@Dillons-iMac.local>
2019-08-05 11:24:24 -0600

    delete functionality made

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {withRouter} from "react-router";
 import {NavLink} from 'react-router-dom';
 
@@ -53,7 +54,9 @@ const NavigationComponent = props => { //functional component
             <div className="right-side">
             CADEN MEYER
             
-            {props.loggedInStatus === 'LOGGED_IN' ? <a onClick={handleSignOut}>Sign Out</a> : null}
+            {props.loggedInStatus === 'LOGGED_IN' ? <a onClick={handleSignOut}>
+                <FontAwesomeIcon icon="sign-out-alt"/>
+            </a> : null}
             </div>
         
         </div>
