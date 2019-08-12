b7d571543c1961cac73645e0e672350abdc4346a
Michelle <dillonmeyer@Dillons-iMac.local>
2019-08-11 21:25:45 -0600

    blog item

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,15 +1,46 @@
-import React from "react";
+import React, {Component} from "react";
 import {Link} from "react-router-dom";
+import axios from 'axios';
+import BlogItem from "../blog/blog-item";
 
-export default function() {
+class Blog extends Component { 
+    constructor(){
+        super();
+
+        this.state = {
+            blogItems: []
+        }
+
+        this.getBlogItems = this.getBlogItems.bind(this);
+    }
+
+    getBlogItems(){
+        axios.get("https://cadenmeyer.devcamp.space/portfolio/portfolio_blogs", {withCredentials: true 
+    }).then(response => {
+        this.setState({
+            blogItems: response.data.portfolio_blogs
+        })
+    }).catch(error => {
+        console.log("getBolgItems error", error);
+    })
+
+    }
+
+    componentWillMount(){
+        this.getBlogItems();
+    }
+
+    render(){
+        const blogRecords = this.state.blogItems.map(blogItem =>{
+            return <BlogItem key={blogItem.id} blogItem={blogItem} />
+        })
+
     return(
         <div>
-           <h2>Blog</h2>
-
-           <div>
-               <Link to="/about-me">Read more about me</Link>
-           </div>
+           {blogRecords};
         </div>
     );
 }
+}
 
+export default Blog;
