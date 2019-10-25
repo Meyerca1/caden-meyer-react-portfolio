562202c8faff83d3b475491efd914b4302cf52d3
Michelle <dillonmeyer@Dillons-iMac.local>
2019-10-25 10:37:20 -0600

    adding rTE styles

---
+++ src/components/forms/rich-text-editor.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class RichTextEditor extends Component {
+    render(){
+        return (
+            <div>
+                <h1>RichTextEditor</h1>
+                <h1>RichTextEditor</h1>
+            </div>
+        )
+    }
+}
