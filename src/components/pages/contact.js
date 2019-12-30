091305aa35f9758f799509d61a397fa72af64946
Michelle <dillonmeyer@Dillons-iMac.local>
2019-12-29 17:25:03 -0700

    final Contact page

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,10 +1,44 @@
 import React from "react";
+import contactImage from "../../../static/assets/images/contact/contact.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function() {
     return(
-        <div>
-            Contact
+        <div className = "contact-page-wrapper">
+        <div 
+        className = "left-column"
+        style= {{
+            background: "url(" + contactImage + ") no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center"
+
+        }}
+        />
+
+        <div className = "right-column">
+       <div className = "contact-bullet-points">
+            <div className ="bullet-point-group">
+                <div className = "icon">
+                <FontAwesomeIcon icon="phone"/></div>
+                <div className = "text">555-555-5555</div>
+            </div>
+
+            <div className ="bullet-point-group">
+                <div className = "icon">
+                <FontAwesomeIcon icon="map-pin"/></div>
+                <div className = "text">Anywhere, UT</div>
+            </div>
+
+            <div className ="bullet-point-group">
+                <div className = "icon">
+                <FontAwesomeIcon icon="clock"/></div>
+                <div className = "text">9AM - 5PM</div>
+            </div>
+       </div>
+     
         </div>
+
+    </div>
     );
 }
 
--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -3,12 +3,17 @@ faTrash,
  faSignOutAlt,
   faEdit,
    faSpinner,
-    faPlusCircle
+    faPlusCircle,
+    faPhone,
+    faMapPin,
+    faClock
+
+
  } from "@fortawesome/free-solid-svg-icons";
  import { library } from "@fortawesome/fontawesome-svg-core";
 
 const Icons = () => {
-return library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle);
+return library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle, faPhone, faMapPin, faClock);
 };
 
 export default Icons;
